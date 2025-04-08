9b620f57778bce9cda68cc873c294e8b5d959d36
Nadjib-coder <nadjib.programmation1@gmail.com>
2025-04-08 18:30:57 +0100

    complet section 04 side effect, next step: start doing project in section 5

--- 04. Side Effects/20. useEffect practice - scrollIntoView()/components/Main.jsx
+++ 04. Side Effects/20. useEffect practice - scrollIntoView()/components/Main.jsx
@@ -1,53 +1,64 @@
-import React from "react"
-import IngredientsList from "./IngredientsList"
-import ClaudeRecipe from "./ClaudeRecipe"
-import { getRecipeFromChefClaude, getRecipeFromMistral } from "../ai"
+import React from 'react';
+import { useState, useEffect, useRef } from 'react';
+import IngredientsList from './IngredientsList';
+import ClaudeRecipe from './ClaudeRecipe';
+import { getRecipeFromChefClaude, getRecipeFromMistral } from '../ai';
 
 export default function Main() {
-    const [ingredients, setIngredients] = React.useState(
-        ["chicken", "all the main spices", "corn", "heavy cream", "pasta"]
-    )
-    const [recipe, setRecipe] = React.useState("")
-    const recipeSection = React.useRef(null)
-    /**
-     * Problem:
-     * We want to scroll the "Ready for a recipe?" div into view
-     * ONLY AFTER the ClaudeRecipe section is rendered to the page 
-     * (i.e. when `recipe` is not an empty string). How can we do that?
-     */
-    
-
-    async function getRecipe() {
-        const recipeMarkdown = await getRecipeFromChefClaude(ingredients)
-        setRecipe(recipeMarkdown)
+  const [ingredients, setIngredients] = React.useState([
+    'chicken',
+    'all the main spices',
+    'corn',
+    'heavy cream',
+    'pasta',
+  ]);
+  const [recipe, setRecipe] = useState('');
+  const recipeSection = useRef(null);
+  /**
+   * Problem:
+   * We want to scroll the "Ready for a recipe?" div into view
+   * ONLY AFTER the ClaudeRecipe section is rendered to the page
+   * (i.e. when `recipe` is not an empty string). How can we do that?
+   */
+  useEffect(() => {
+    if (recipe && recipeSection) {
+      recipeSection.current.scrollIntoView({
+        behavior: 'smooth',
+      });
     }
+  }, [recipe]);
 
-    function addIngredient(formData) {
-        const newIngredient = formData.get("ingredient")
-        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
-    }
-    
-    return (
-        <main>
-            <form action={addIngredient} className="add-ingredient-form">
-                <input
-                    type="text"
-                    placeholder="e.g. oregano"
-                    aria-label="Add ingredient"
-                    name="ingredient"
-                />
-                <button>Add ingredient</button>
-            </form>
+  async function getRecipe() {
+    const recipeMarkdown = await getRecipeFromChefClaude(ingredients);
+    setRecipe(recipeMarkdown);
+  }
 
-            {ingredients.length > 0 &&
-                <IngredientsList
-                    ref={recipeSection}
-                    ingredients={ingredients}
-                    getRecipe={getRecipe}
-                />
-            }
+  function addIngredient(formData) {
+    const newIngredient = formData.get('ingredient');
+    setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
+  }
 
-            {recipe && <ClaudeRecipe recipe={recipe} />}
-        </main>
-    )
+  return (
+    <main>
+      <form action={addIngredient} className="add-ingredient-form">
+        <input
+          type="text"
+          placeholder="e.g. oregano"
+          aria-label="Add ingredient"
+          name="ingredient"
+        />
+        <button>Add ingredient</button>
+      </form>
+
+      {ingredients.length > 0 && (
+        <IngredientsList
+          ref={recipeSection}
+          ingredients={ingredients}
+          getRecipe={getRecipe}
+        />
+      )}
+
+      {recipe && <ClaudeRecipe recipe={recipe} />}
+    </main>
+  );
 }
